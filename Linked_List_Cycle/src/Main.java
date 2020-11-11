6286ca46902d0be20b5ca5b54456293219598aeb
Ertjon <ertjonmecka@gmail.com>
2020-11-11 20:20:40 +0100

    Linked List Cycle

---
+++ Linked_List_Cycle/src/Main.java
@@ -0,0 +1,49 @@
+/*
+https://leetcode.com/problems/linked-list-cycle/
+ */
+
+public class Main {
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public static boolean hasCycle(ListNode head) {
+        if (head == null)
+            return false;
+        if (head.next == null)
+            return false;
+        if (head.next.next == null)
+            return false;
+
+        ListNode slowPointer = head;
+        ListNode fastPointer = head.next;
+        while (slowPointer != fastPointer) {
+            if (fastPointer == null || fastPointer.next == null) {
+                return false;
+            }
+            slowPointer = slowPointer.next;
+            fastPointer = fastPointer.next.next;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(3);
+        ListNode l2 = new ListNode(2);
+        ListNode l3 = new ListNode(0);
+        ListNode l4 = new ListNode(-4);
+
+        l1.next = l2;
+        l2.next = l3;
+        l3.next = l4;
+        l4.next = l2;
+
+        System.out.println(hasCycle(l1));
+    }
+}
