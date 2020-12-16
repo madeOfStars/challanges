537454bad5b64ed42a2bbaf7d29556afe073d39e
Ertjon Mecka <ertjon.mecka@h-d-gmbh.de>
2020-12-16 17:16:15 +0100

    initial commit for tree serialization

---
+++ learning/src/trees/SerializeAndDeserializeBinaryTree.java
@@ -0,0 +1,23 @@
+package trees;
+
+public class SerializeAndDeserializeBinaryTree {
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        if (root == null)
+            return "";
+
+
+
+        return null;
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
