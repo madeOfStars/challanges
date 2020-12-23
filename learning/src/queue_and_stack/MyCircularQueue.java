c3dca7b6d760b2c6bbad4d3b7aebff4f05dadc74
Ertjon Mecka <ertjon.mecka@h-d-gmbh.de>
2020-12-23 16:30:47 +0100

    Design Circular Queue

--- learning/src/queue_and_stack/MyCircularQueue.java
+++ learning/src/queue_and_stack/MyCircularQueue.java
@@ -1,16 +1,17 @@
+/*
+https://leetcode.com/problems/design-circular-queue/
+ */
+
 package queue_and_stack;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class MyCircularQueue {
-    private final List<Integer> data;
+    private final int[] data;
     private final int size;
     private int head;
     private int tail;
 
     public MyCircularQueue(int k) {
-        data = new ArrayList<>(k);
+        data = new int[k];
         size = k;
         head = -1;
         tail = -1;
@@ -23,7 +24,7 @@ public class MyCircularQueue {
         if (isEmpty())
             head++;
         tail = (tail + 1) % size;
-        data.add(tail, value);
+        data[tail] = value;
 
         return true;
     }
@@ -44,14 +45,14 @@ public class MyCircularQueue {
         if (isEmpty())
             return -1;
 
-        return data.get(head);
+        return data[head];
     }
 
     public int Rear() {
         if (isEmpty())
             return -1;
 
-        return data.get(tail);
+        return data[tail];
     }
 
     public boolean isEmpty() {
@@ -63,30 +64,40 @@ public class MyCircularQueue {
     }
 
     public static void main(String[] args) {
-        MyCircularQueue obj = new MyCircularQueue(2);
-        boolean param_1 = obj.enQueue(1);
-        boolean param_11 = obj.enQueue(2);
+        MyCircularQueue obj = new MyCircularQueue(6);
+        boolean param_1 = obj.enQueue(6);
+        int param_4 = obj.Rear();
+        int param_41 = obj.Rear();
         boolean param_2 = obj.deQueue();
-        boolean param_12 = obj.enQueue(3);
+        boolean param_11 = obj.enQueue(5);
+        int param_42 = obj.Rear();
         boolean param_21 = obj.deQueue();
-        boolean param_13 = obj.enQueue(3);
-        boolean param_22 = obj.deQueue();
-        boolean param_14 = obj.enQueue(3);
-        boolean param_23 = obj.deQueue();
         int param_3 = obj.Front();
+        boolean param_22 = obj.deQueue();
+        boolean param_23 = obj.deQueue();
+        boolean param_24 = obj.deQueue();
+
+        boolean param_12 = obj.enQueue(3);
+        boolean param_13 = obj.enQueue(3);
+        boolean param_14 = obj.enQueue(3);
         /*int param_4 = obj.Rear();
         boolean param_5 = obj.isEmpty();
         boolean param_6 = obj.isFull();*/
 
         System.out.println(param_1);
-        System.out.println(param_11);
+        System.out.println(param_4);
+        System.out.println(param_41);
         System.out.println(param_2);
-        System.out.println(param_12);
+        System.out.println(param_11);
+        System.out.println(param_42);
         System.out.println(param_21);
-        System.out.println(param_13);
-        System.out.println(param_22);
-        System.out.println(param_14);
-        System.out.println(param_23);
         System.out.println(param_3);
+        System.out.println(param_22);
+        System.out.println(param_23);
+        System.out.println(param_24);
+        /*System.out.println(param_12);
+        System.out.println(param_13);
+        System.out.println(param_14);
+        System.out.println(param_23);*/
     }
 }
