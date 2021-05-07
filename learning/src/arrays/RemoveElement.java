30027698dccb5cdde48a40a82011d856dbaa9c2b
Ertjon Mecka <ertjon.mecka@h-d-gmbh.de>
2021-05-07 17:12:18 +0200

    Remove Element

---
+++ learning/src/arrays/RemoveElement.java
@@ -0,0 +1,29 @@
+/*
+https://leetcode.com/problems/remove-element/
+ */
+package arrays;
+
+import java.util.Arrays;
+
+public class RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        int k = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+
+    public static void main(String[] args) {
+        RemoveElement re = new RemoveElement();
+
+        int[] nums = new int[]{3, 2, 2, 3};
+        System.out.println(re.removeElement(nums, 3));
+        System.out.println(Arrays.toString(nums));
+    }
+}
