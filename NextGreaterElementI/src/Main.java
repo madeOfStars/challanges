4474a1a66d78ae8ef967ed7d4114ccf15b331c95
ertjon_mecka <ertjon.mecka@sulzer-us.com>
2021-10-19 23:47:53 +0200

    Next Greater Element I

---
+++ NextGreaterElementI/src/Main.java
@@ -0,0 +1,39 @@
+/*
+https://leetcode.com/problems/next-greater-element-i/
+ */
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class Main {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Stack<Integer> stack = new Stack<>();
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums2) {
+            while (!stack.empty() && num > stack.peek())
+                map.put(stack.pop(), num);
+            stack.push(num);
+        }
+
+        while (!stack.empty())
+            map.put(stack.pop(), -1);
+
+        int[] res = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            res[i] = map.get(nums1[i]);
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Main m = new Main();
+
+        System.out.println(Arrays.toString(m.nextGreaterElement(new int[]{4, 1, 2}, new int[]{1, 3, 4, 2})));
+        System.out.println(Arrays.toString(m.nextGreaterElement(new int[]{2, 4}, new int[]{1, 2, 3, 4})));
+        System.out.println(Arrays.toString(m.nextGreaterElement(new int[]{3, 1, 5, 7, 9, 2, 6}, new int[]{1, 2, 3, 5, 6, 7, 9, 11})));
+        System.out.println(Arrays.toString(m.nextGreaterElement(new int[]{1, 3, 5, 2, 4}, new int[]{6, 5, 4, 3, 2, 1, 7})));
+    }
+}
