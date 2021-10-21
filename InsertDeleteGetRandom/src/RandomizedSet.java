b3e9ff745509acddc983ac2fd8cab6422ad87d8f
ertjon_mecka <ertjon.mecka@sulzer-us.com>
2021-10-21 22:13:35 +0200

    Insert Delete GetRandom O(1)

---
+++ InsertDeleteGetRandom/src/RandomizedSet.java
@@ -0,0 +1,66 @@
+/*
+https://leetcode.com/problems/insert-delete-getrandom-o1/
+ */
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+public class RandomizedSet {
+    private final Map<Integer, Integer> map;
+    private final List<Integer> list;
+    private final Random random = new Random();
+
+    public RandomizedSet() {
+        map = new HashMap<>();
+        list = new ArrayList<>();
+    }
+
+    public boolean insert(int val) {
+        if (map.containsKey(val))
+            return false;
+
+        map.put(val, list.size());
+        list.add(val);
+        return true;
+    }
+
+    public boolean remove(int val) {
+        if (!map.containsKey(val))
+            return false;
+
+        Integer lastElement = list.get(list.size() - 1);
+        list.set(map.get(val), lastElement);
+        map.put(lastElement, map.get(val));
+        map.remove(val);
+        list.remove(list.size() - 1);
+
+        return true;
+    }
+
+    public int getRandom() {
+        return list.get(random.nextInt(list.size()));
+    }
+
+    public static void main(String[] args) {
+        RandomizedSet randomizedSet = new RandomizedSet();
+        /*System.out.println(randomizedSet.insert(1)); // Inserts 1 to the set. Returns true as 1 was inserted successfully.
+        System.out.println(randomizedSet.remove(2)); // Returns false as 2 does not exist in the set.
+        System.out.println(randomizedSet.insert(2)); // Inserts 2 to the set, returns true. Set now contains [1,2].
+        System.out.println(randomizedSet.getRandom()); // getRandom() should return either 1 or 2 randomly.
+        System.out.println(randomizedSet.remove(1)); // Removes 1 from the set, returns true. Set now contains [2].
+        System.out.println(randomizedSet.insert(2)); // 2 was already in the set, so return false.
+        System.out.println(randomizedSet.getRandom());*/
+
+        System.out.println("--------------------------");
+
+        randomizedSet = new RandomizedSet();
+        System.out.println(randomizedSet.insert(0));
+        System.out.println(randomizedSet.insert(1));
+        System.out.println(randomizedSet.remove(0));
+        System.out.println(randomizedSet.insert(2));
+        System.out.println(randomizedSet.remove(1));
+        System.out.println(randomizedSet.getRandom());
+    }
+}
