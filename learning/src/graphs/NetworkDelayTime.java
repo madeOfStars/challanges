027467f767056bab6182591985243f628fff0f99
ertjon_mecka <meckaertjon@gmail.com>
2022-09-18 18:53:06 +0200

    Network Delay Time

---
+++ learning/src/graphs/NetworkDelayTime.java
@@ -0,0 +1,97 @@
+/*
+https://leetcode.com/problems/network-delay-time/
+ */
+
+package graphs;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+
+public class NetworkDelayTime {
+    private record Pair(Integer left, Integer right) {}
+
+    public int networkDelayTime(int[][] times, int n, int k) {
+        Map<Integer, List<Pair>> adj = new HashMap<>();
+
+        for (int[] time : times) {
+            adj.putIfAbsent(time[0], new ArrayList<>());
+            adj.get(time[0]).add(new Pair(time[1], time[2]));
+        }
+
+        int[] dist = new int[n + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+
+        Queue<Pair> queue = new PriorityQueue<>(Comparator.comparingInt(a -> a.right));
+        Set<Integer> visited = new HashSet<>();
+        queue.add(new Pair(k, 0));
+        visited.add(k);
+        dist[k] = 0;
+
+        while (!queue.isEmpty()) {
+            var polledNode = queue.poll();
+
+            if (polledNode.right > dist[polledNode.left]) {
+                continue;
+            }
+
+            if (!adj.containsKey(polledNode.left)) {
+                continue;
+            }
+
+            visited.add(polledNode.left);
+
+            for (var nextNode : adj.get(polledNode.left)) {
+                if (visited.contains(nextNode)) {
+                    continue;
+                }
+
+                var neighbor = nextNode.left;
+                var time = nextNode.right;
+
+                if (polledNode.right + time < dist[neighbor]) {
+                    dist[neighbor] = polledNode.right + time;
+                    queue.add(new Pair(neighbor, dist[neighbor]));
+                }
+            }
+        }
+
+        int answer = Integer.MIN_VALUE;
+        for (int i = 1; i <= n; i++) {
+            answer = Math.max(answer, dist[i]);
+        }
+
+        return answer == Integer.MAX_VALUE ? -1 : answer;
+    }
+
+    public static void main(String[] args) {
+        NetworkDelayTime m = new NetworkDelayTime();
+
+        int[][] times1 = {
+                {2, 1, 1},
+                {2, 3, 1},
+                {3, 4, 1}
+        };
+
+        System.out.println(m.networkDelayTime(times1, 4, 2)); //2
+
+        int[][] times2 = {
+                {1, 2, 1},
+        };
+
+        System.out.println(m.networkDelayTime(times2, 2, 1)); //1
+
+        int[][] times3 = {
+                {1, 2, 1},
+        };
+
+        System.out.println(m.networkDelayTime(times3, 2, 2)); //-1
+    }
+}
